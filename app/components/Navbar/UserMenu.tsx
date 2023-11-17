8297ed8ef0e2cbb6b61a2fc88503fff1b41d6963
prabhjot <psingh20112002@gmail.com>
2023-11-17 22:29:39 +0530

    bug fix

--- app/components/Navbar/UserMenu.tsx
+++ app/components/Navbar/UserMenu.tsx
@@ -57,7 +57,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
                 >
                     Lodgify your home
                 </div>
-                <div onClick={toggleOpen()}
+                <div onClick={toggleOpen}
                     className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
                 >
                     <AiOutlineMenu />
