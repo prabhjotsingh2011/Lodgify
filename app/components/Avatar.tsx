9a06bd98b517a24ae987a77c0b798db0a871a088
prabhjot <psingh20112002@gmail.com>
2023-11-17 22:44:24 +0530

    bug fix 2

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -1,22 +1,17 @@
 'use client'
 
 import Image from "next/image";
-interface AvatarProps {
-    src?: string | null;
-}
 
-const Avatar:React.FC<AvatarProps>=({
-    src
-})=> {
+function Avatar() {
     return (
         <Image 
             className='rounded-full'
             height='30'
             width='30'
             alt='avatar'
-            src={src ? src :  '/images/placeholder.jpg'}
+            src='/images/placeholder.jpg'
         />
     );
-}   
+}
 
 export default Avatar;
--- app/components/listings/ListingInfo.tsx
+++ app/components/listings/ListingInfo.tsx
@@ -44,7 +44,6 @@ const ListingInfo: React.FC<ListingInfoProps> = ({
                 <div className="text-xl font-semibold flex flex-row items-center gap-2">
                     <div> Hosted by {user?.name} </div>
                     <Avatar
-                        src={user?.image}
                     />
                 </div>
                 <div className="flex flex-row items-center gap-4 font-light text-neutral-500">
